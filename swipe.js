ab9bf9344e37a75c94dfa9de49a122b9670d0495
Mark Ziecina <ziecina@gmail.com>
2014-10-31 23:13:23 -0400

    .change is works

--- swipe.js
+++ swipe.js
@@ -17,12 +17,23 @@ $( document ).on( "pagecreate", "#demo-page", function() {
         if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
             if ( e.type === "swipeleft" ) {
 				var row = $(e.target).closest("tr");
-				console.log(row.children("td:nth-child(1)").text());
 				$( "#right-panel input[id='textinput-1']").val(row.children("td:nth-child(1)").text());
+				$( "#right-panel input[id='textinput-1']").attr("refid", row.children("td:nth-child(1)").attr("id"));
+
 				$( "#right-panel input[id='textinput-2']").val(row.children("td:nth-child(2)").text());
+				$( "#right-panel input[id='textinput-2']").attr("refid", row.children("td:nth-child(2)").attr("id"));
                 $( "#right-panel" ).panel( "open" );
             }
         }
     });
+	
+	$( "#right-panel input").change(function(){
+		var refid = $(this).attr("refid");
+		$("#" + refid).text($(this).val());
+		
+		var row = $("#" + refid).closest("tr");
+		row.children("td:nth-child(3)").text(parseFloat(row.children("td:nth-child(2)").text()) / parseFloat(row.children("td:nth-child(1)").text()));
+	});
+	
 });
 
