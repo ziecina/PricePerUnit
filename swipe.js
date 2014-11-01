9d0734c4d606d5ea86406baaa5fd5f334b2db6cc
Mark Ziecina <ziecina@gmail.com>
2014-10-31 22:36:54 -0400

    swiping for adding/editing is added

--- swipe.js
+++ swipe.js
@@ -1,5 +1,5 @@
 $( document ).on( "pagecreate", "#demo-page", function() {
-    $( document ).on( "swipeleft swiperight", "#demo-page", function( e ) {
+    $( document ).on( "swiperight", "#demo-page", function( e ) {
         // We check if there is no open panel on the page because otherwise
         // a swipe to close the left panel would also open the right panel (and v.v.).
         // We do this by checking the data that the framework stores on the page element (panel: open).
@@ -9,4 +9,20 @@ $( document ).on( "pagecreate", "#demo-page", function() {
             }
         }
     });
+	
+	$( document ).on( "swipeleft", "#mainTable1", function( e ) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
+            if ( e.type === "swipeleft" ) {
+				var row = $(e.target).closest("tr");
+				console.log(row.children("td:nth-child(1)").text());
+				$( "#right-panel input[id='textinput-1']").val(row.children("td:nth-child(1)").text());
+				$( "#right-panel input[id='textinput-2']").val(row.children("td:nth-child(2)").text());
+                $( "#right-panel" ).panel( "open" );
+            }
+        }
+    });
 });
+
